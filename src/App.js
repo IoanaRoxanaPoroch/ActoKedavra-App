241266983c890293cff9bde42730c4af7b36cac5
Poroch Ioana <ioanaroxanaporoch@gmail.com>
2022-04-02 13:32:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const actor = {
+    name: "Leornardo Dicaprio",
+    score: 10,
+    hobbies: "Music and dancing naked in the rain",
+    description: "He is a good guy with a thick mustache",
+  };
+  const arr = [1, 2, 3];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="actor">
+        <ul>
+          {/* {for(const prop in actor)
+            {
+              <li>
+                {`${prop}:${actor[prop]}`}
+              </li>
+            }} */}
+        </ul>
+      </div>
     </div>
   );
 }
