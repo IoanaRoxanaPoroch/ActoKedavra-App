9cd7f1d3b508befa7c43a899399834565babe1a1
Poroch Ioana <ioanaroxanaporoch@gmail.com>
2022-04-04 14:29:19 +0300

    using destructuring props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import "./App.css";
 import Actor from "./components/actor/Actor";
 
 function App() {
+  const actor = {
+    name: "Leonardo Dicaprio",
+    score: 10,
+    hobbies: "Music and dancing naked in the rain",
+    description: "He is a good guy with a thick mustache",
+  };
   return (
     <div className="App">
-      <Actor />
+      <Actor actor={actor} />
     </div>
   );
 }
--- src/components/actor/Actor.js
+++ src/components/actor/Actor.js
@@ -1,19 +1,9 @@
-const Actor = () => {
-  const actor = {
-    name: "Leonardo Dicaprio",
-    score: 10,
-    hobbies: "Music and dancing naked in the rain",
-    description: "He is a good guy with a thick mustache",
-  };
-
+const Actor = ({ actor }) => {
+ 
   const listItems = Object.keys(actor).map((key) => (
     <li key={key}>{actor[key]}</li>
   ));
 
-  // let listItems1 = [];
-  // for (const [key, value] of Object.entries(actor)) {
-  //   listItems1.push(<li key={key}>{value}</li>);
-  // }
 
   return (
     <div>
@@ -25,3 +15,4 @@ const Actor = () => {
 };
 
 export default Actor;
+
